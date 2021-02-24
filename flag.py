a328862c2e444a2dfcbae55ff659105e12bc0ae5
Franc Jerez <francjerez@users.noreply.github.com>
2021-02-24 16:47:30 +0100

    Update flag.py
    Autodetect unplugging/replugging.

--- flag.py
+++ flag.py
@@ -5,32 +5,37 @@ import hid
 import time
 import subprocess
 
-dev = hid.device() 
+vid = 0x04d8#  1240
+pid = 0xf372# 62322
+dev = hid.device()
 
 def col(flg):
-  dev.open(0x04d8, 0xf372)
+  dev.open(vid, pid)
   if flg == 1:
-    dev.write([0, 0, 82, 0, 0, 0, 0, 0])# Red    (busy)
+    dev.write([0, 0, 66, 0, 0, 0, 0, 0])# Blue   (away)
   if flg == 2:
     dev.write([0, 0, 71, 0, 0, 0, 0, 0])# Green  (free)
   if flg == 3:
-    dev.write([0, 0, 66, 0, 0, 0, 0, 0])# Blue   (away)
+    dev.write([0, 0, 82, 0, 0, 0, 0, 0])# Red    (busy)
   dev.close()
 
 # Off
 if sys.argv[-1] == "logout":
-  col(3)
+  col(1)
 
 # On
 else:
-  flg = 3
-  pre =-1
+  pre = 0
   while True:
     time.sleep(0.5)
 
+    # Flag is unplugged
+    if not hid.enumerate(vid, pid): 
+      flg = 0
+
     # Display is sleeping
-    if b"Asleep" in subprocess.check_output(['system_profiler', 'SPDisplaysDataType']):
-      flg = 3
+    elif b"Asleep" in subprocess.check_output(['system_profiler', 'SPDisplaysDataType']):
+      flg = 1
 
     # DND is disabled
     elif subprocess.check_output(['defaults', 'read', 'com.apple.ncprefs.plist', 'dnd_prefs'])[-11:-3] == b'0000007c':
@@ -38,8 +43,8 @@ else:
 
     # DND is enabled
     else:
-      flg = 1
+      flg = 3
 
-    if pre != flg:
+    if flg and flg != pre:
       col(flg)
     pre = flg 
